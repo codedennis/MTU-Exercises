0e797a91e0e5258ef0360573f367a2466bb67167
Dennis Santos <59423597+codedennis@users.noreply.github.com>
2023-11-15 20:38:14 +0000

    Lecture

--- ProgrammingFundamentals/Week 3/lecture.py
+++ ProgrammingFundamentals/Week 3/lecture.py
@@ -6,3 +6,15 @@ print(name , 'is ', age , 'and', weight_in_kg, 'kg')
 print(name , 'is a ', type(name))
 print(age , type(age))
 print(weight_in_kg, 'is a', type(weight_in_kg))
+
+#Get the users first name
+first_name = input('Enter your first name: ')
+
+#Get the users last name
+last_name = input('Enter your first name: ')
+
+print(type(first_name))
+print(type(last_name))
+
+#print a greeting to the user
+print('Hello', first_name, last_name)
