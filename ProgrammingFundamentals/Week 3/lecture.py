2161f19ea20e0724da987645b79275a8149ce440
Dennis Santos <59423597+codedennis@users.noreply.github.com>
2023-11-15 20:31:15 +0000

    Week 3 - Lecture

---
+++ ProgrammingFundamentals/Week 3/lecture.py
@@ -0,0 +1,8 @@
+name = 'John'
+age = 19
+weight_in_kg = 10.45
+
+print(name , 'is ', age , 'and', weight_in_kg, 'kg')
+print(name , 'is a ', type(name))
+print(age , type(age))
+print(weight_in_kg, 'is a', type(weight_in_kg))
